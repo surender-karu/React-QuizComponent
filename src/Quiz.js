8601756b3f34c9dd8f79d4eac2dde9d199d53a3a
surender-karu <k_suren2001@yahoo.com>
2018-07-01 12:28:33 +0800

    Stage1

---
+++ src/Quiz.js
@@ -0,0 +1,23 @@
+import React, { Component } from 'react';
+
+let quizData = require('./quiz_data.json');
+
+class Quiz extends Component {
+    constructor() {
+        super(props);
+
+        this.state = {
+            quiz_position: 1
+        };
+    }
+
+    render() {
+        return (
+            <div>
+                <div className="QuizQuestion">
+                    {quizData[this.state.quiz_position].instruction_text}
+                </div>
+            </div>
+        )
+    }
+}
